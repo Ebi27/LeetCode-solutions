2f41ce57aa04a9ee8a4b6e5c0153cacc25408b7c
Ebi Kpemi-Ogokimi <kpemiogokimiebi@gmail.com>
2022-05-16 21:11:40 +0200

    Time: 294 ms (15.22%), Space: 50.8 MB (59.80%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    nums.sort();
+    
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] === nums[i + 1]) 
+            return true;
+    }
+    return false;
+};
+
+//time complexity O(n) - because we searched linearly through the nums array 
+//space complexity O(1) - we maintained the same space 
