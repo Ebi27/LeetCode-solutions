2975514b23dd0301d71f7ac6bc1ef67ce1bfb8c2
Ebi Kpemi-Ogokimi <kpemiogokimiebi@gmail.com>
2022-05-23 19:23:30 +0200

    Time: 198 ms (14.03%), Space: 54.7 MB (55.95%) - LeetHub

--- 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
@@ -30,3 +30,21 @@ var findMaxAverage = function(nums, k) {
     }
     return kSum/k;
 };
+
+
+
+//Another method 
+// var findMaxAverage = function(nums, k) {
+//     let sum = 0,
+//         maxSum = 0;
+//     for(let i = 0; i < k; ++i) {
+//         sum += nums[i];
+//     }
+//     maxSum = sum;
+    
+//     for(let i = k; i < nums.length; ++i) {
+//         sum = sum - nums[i - k] + nums[i];
+//         maxSum = Math.max(maxSum, sum);
+//     }
+//     return maxSum / k;
+// };
