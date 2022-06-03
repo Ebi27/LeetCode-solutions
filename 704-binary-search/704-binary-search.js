0ed2af106e4dd97430085c65663c0d2db1bb7aba
Ebi Kpemi-Ogokimi <kpemiogokimiebi@gmail.com>
2022-06-03 12:56:31 +0200

    Time: 62 ms (95.88%), Space: 44.9 MB (71.97%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,20 +4,19 @@
  * @return {number}
  */
 var search = function(nums, target) {
-let left = 0; 
-let right = nums.length - 1;
+let left = 0;
+    let right = nums.length -1;
     
-    while(left <= right){ 
-      const mid = Math.floor((left + right)/2);
-        if(nums[mid] == target){
-            return mid ; 
-        }else if (nums[mid] < target){
-            left = mid + 1;
-        }else if (nums[mid] > target){
-            right = mid - 1;
-        }
+   while(left <= right){
+        const mid = Math.floor((left + right) /2);
+        if(nums[mid] < target) 
+            left = mid + 1
+       else right = mid -1;
+         if(nums[mid] === target){ 
+        return mid;
+         }
     }
-    return -1;
+     return -1;
 };
- //time complexity O(log n)
+ //time complexity O(n)
 //Space complexity O(n)
