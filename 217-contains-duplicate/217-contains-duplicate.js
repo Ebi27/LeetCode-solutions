69cd5498e5bfd479a823541f8a1ab59c28031f00
Ebi Kpemi-Ogokimi <kpemiogokimiebi@gmail.com>
2022-06-17 22:00:42 +0200

    Time: 193 ms (24.66%), Space: 50.9 MB (57.14%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,11 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums.sort();
+nums.sort();
     
-    for (let i = 0; i < nums.length; i++){
-        if (nums[i] === nums[i + 1]) 
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] == nums[i + 1]){
             return true;
+        }
     }
     return false;
 };
