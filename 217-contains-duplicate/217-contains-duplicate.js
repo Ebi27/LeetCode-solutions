9a3f6dabb9f7f1a62181c209458606c0502d0d9a
Ebi Kpemi-Ogokimi <kpemiogokimiebi@gmail.com>
2022-06-20 18:31:06 +0200

    Time: 318 ms (15.54%), Space: 50.8 MB (63.68%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -2,16 +2,26 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
+
+// sort the numbers 
+//unsorted = [1,2,3,1]
+//sorted = [1,1,2,3]
+//loop through the sorted nums 
+// take our first element and check if its equal to our second element 
+// if it is, then we return true , else false 
+
+
 var containsDuplicate = function(nums) {
-nums.sort();
+nums.sort(); //o(n log n )
     
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] == nums[i + 1]){
-            return true;
-        }
+    
+    for(let i = 0; i <nums.length; i++){  //o(n)
+        if(nums[i] === nums[i + 1]) return true; //o(1)
     }
-    return false;
+    return false; //o(n)
 };
 
-//time complexity O(n) - because we searched linearly through the nums array 
+
+//time complexity o(n log n)
 //space complexity O(1) - we maintained the same space 
