e06d288403c9f891ba9da889d7e854b22c6a8c93
Ebi Kpemi-Ogokimi <kpemiogokimiebi@gmail.com>
2022-06-27 14:16:52 +0200

    Time: 102 ms (34.23%), Space: 42.9 MB (60.00%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+
+//declare start point 
+//declare end point 
+//loop through 
+//declare mid point 
+//check if mid is less than target, increment start point 
+//check if mid is greater than, decrement end point 
+// if equal then return the index of both numbers that add up to target number plus 1 'added by one'
+//else return -1 
+
+//remember it is 1 indexed 
+
+const twoSum = (numbers, target) => {
+    let left = 0;
+    let right = numbers.length - 1;
+    
+    while (left < right) {
+        let sum = numbers[left] + numbers[right];
+        
+        if (sum === target) {
+            return [left + 1, right + 1];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+};
