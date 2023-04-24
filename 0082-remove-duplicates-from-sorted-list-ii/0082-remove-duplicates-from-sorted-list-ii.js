ab0f8ba497900730cbdad98b2f63c2ee3fc05ff2
Ebi Kpemi-Ogokimi <kpemiogokimiebi@gmail.com>
2023-04-24 19:55:45 +0200

    Time: 61 ms (80.31%), Space: 44.1 MB (80.83%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
@@ -10,16 +10,25 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-if(head==null || head.next==null) return head;
-
+    
+if(head==null || head.next==null) return head; //base case 
+//[1,2,3,3,4,4,5]
+// c  c.n 
     let current = head;
     
+    //This returns the value in the node if it's not a duplicate. 
 if(current.next.val != current.val){
-current.next = deleteDuplicates(current.next);
+ current.next = deleteDuplicates(current.next); //computes the element in the node until it computes the entire list 
 return current;
+    
 }
     
+    //This reassigns pointer when there is a duplicate 
 let itr = current.next;
-while(itr != null && itr.val == current.val) itr = itr.next;
+while(itr != null && itr.val == current.val) itr = itr.next; // 
 return deleteDuplicates(itr);
 };
+
+// while(curr !-- null){
+//     curr = head.next  
+// }
