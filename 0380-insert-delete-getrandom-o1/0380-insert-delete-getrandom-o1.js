b09baebad08d5567c8ee5f15642c2751c5355323
Ebi Kpemi-Ogokimi <kpemiogokimiebi@gmail.com>
2023-06-26 21:18:55 +0200

    Time: 1011 ms (5.66%), Space: 120.2 MB (6.60%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.js
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.js
@@ -1,6 +1,16 @@
+/*
+-Store the data in an array and use a hashmap to track the location of each element in the array 
+- use the hashmap to lookup the location of the element to delete 
+- Swap the element in the array with the one to be deleted 
+- Update the location of the moved element in the hashmap 
+- Delete the key-value pair of the target data element from the array. 
+*/
+
+
 var RandomizedSet = function() {
-    this.map = new Map()
-    this.arr = []
+    this.hashmap = new Map();
+    this.arr = []; 
+ 
 };
 
 /** 
@@ -8,13 +18,13 @@ var RandomizedSet = function() {
  * @return {boolean}
  */
 RandomizedSet.prototype.insert = function(val) {
-    if(this.map.has(val)){
-      return false  
+    if(!this.hashmap.has(val)){
+        this.arr.push(val)
+       
+        this.hashmap.set(val, this.arr.length-1)
+        return true;
     }
-    this.arr.push(val)
-    let idx = this.arr.length === 0 ? 0 : this.arr.length - 1
-    this.map.set(val, idx)
-    return true
+    return false; 
 };
 
 /** 
@@ -22,6 +32,70 @@ RandomizedSet.prototype.insert = function(val) {
  * @return {boolean}
  */
 RandomizedSet.prototype.remove = function(val) {
+if(!this.hashmap.has(val)){
+      return false;
+      }
+
+else{
+     const currIdx =  this.hashmap.get(val)
+    const lastIdx = this.arr.length-1
+    const lastItem = this.arr[lastIdx]
+    
+    swap(currIdx, lastIdx, this.arr)
+    this.hashmap.set(lastItem, currIdx)
+    this.hashmap.delete(val)
+    this.arr.pop()
+    return true 
+ 
+}
+}
+
+ const swap = (curr, last, arr) => {
+        let temp = arr[curr]
+        arr[curr] = arr[last]
+        arr[last] = temp
+    }
+
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+  let genRandom = Math.floor((Math.random() * this.arr.length)) 
+  console.log(this.arr.length)
+  return this.arr[genRandom]
+};
+
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
+
+// this.map = new Map()
+//     this.arr = []
+// };
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+// RandomizedSet.prototype.insert = function(val) {
+//     if(this.map.has(val)){
+//       return false  
+//     }
+//     this.arr.push(val)
+//     let idx = this.arr.length === 0 ? 0 : this.arr.length - 1  // to get the length of the existing arr, to update the map with the value and index. if the length of arr is 0, then we set the index to 0. If not, get the length o the array - 1 and use that as the index in the map. 
+//     this.map.set(val, idx)  //set value and index in the map 
+//     return true
+// };
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+/**RandomizedSet.prototype.remove = function(val) {
    if(this.map.has(val)) {
         let currIdx = this.map.get(val) // idx of number
         let lastIdx = this.arr.length - 1
@@ -35,6 +109,8 @@ RandomizedSet.prototype.remove = function(val) {
     }
     return false
 };
+// [ 3, 4, 20, 10, 7]
+// 3: 0,  4: 1, 7:2, 10:3, 20:4  
 const swap = (currIdx, lastIdx, arr)=>{
     let temp = arr[currIdx]
     arr[currIdx] = arr[lastIdx]
@@ -45,15 +121,7 @@ const swap = (currIdx, lastIdx, arr)=>{
 /**
  * @return {number}
  */
-RandomizedSet.prototype.getRandom = function() {
-   let random =  Math.floor((Math.random() * this.arr.length))
-   return this.arr[random]
-};
-
-/** 
- * Your RandomizedSet object will be instantiated and called as such:
- * var obj = new RandomizedSet()
- * var param_1 = obj.insert(val)
- * var param_2 = obj.remove(val)
- * var param_3 = obj.getRandom()
- */
+// RandomizedSet.prototype.getRandom = function() {
+//    let random =  Math.floor((Math.random() * this.arr.length))
+//    return this.arr[random]
+    
