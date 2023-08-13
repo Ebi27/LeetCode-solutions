c1733b1352e8553c148096c396a2552087ac6ea7
Ebi Kpemi-Ogokimi <kpemiogokimiebi@gmail.com>
2023-08-13 18:00:11 +0200

    Time: 439 ms (82.54%), Space: 27.3 MB (89.55%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        Input:type nums: List[int]
+       Output:rtype: bool
+        
+        We need to determine if there are any duplicates in our input. 
+        
+        create a dictionary to keep track of the frequency of numbers 
+            loop through the nums list 
+                if the curr num is present in the diction 
+                     it is? then, we have a duplicate. Return true. 
+                else if it isn't, then we add it and assign a freq of 1.      
+            If none meet the duplicate check condition, then we have no duplicate. We return true. 
+        """
+        
+        dict = {}
+        
+        for num in nums:
+            if num in dict:
+                return True
+            elif not num in dict:
+                dict[num] = 1
+        
+        return False
